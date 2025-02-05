b88cf6226a7911d1c238bc0c4a120074f162fdad
Maryam shakeri <89182184+M91skr@users.noreply.github.com>
2025-02-05 12:43:20 +0100

    Update Constants.js
    Improve comments

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,9 +1,13 @@
+// Define the total number of circles on the board
 export const NO_CIRCLES = 16;
+
+// Define player constants for an empty spot, Player 1, and Player 2
 export const NO_PLAYER = 0;
 export const PLAYER_1 = 1;
 export const PLAYER_2 = 2;
 
-export const GAME_STATE_IDLE = 0;
-export const GAME_STATE_PLAYING = 1;
-export const GAME_STATE_WIN = 2;
-export const GAME_STATE_DRAW = 3;
+// Define game state constants
+export const GAME_STATE_IDLE = 0;     // Game is not started
+export const GAME_STATE_PLAYING = 1;  // Game is in progress
+export const GAME_STATE_WIN = 2;      // A player has won
+export const GAME_STATE_DRAW = 3;     // The game ended in a draw
