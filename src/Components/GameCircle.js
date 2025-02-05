7cab43dd12494d5cc64ec795ecefa7388fe5ee48
Maryam shakeri <89182184+M91skr@users.noreply.github.com>
2025-02-05 12:46:13 +0100

    Update GameCircle.js
    Improve comments

--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -1,11 +1,13 @@
+// Import React and styles
 //Write "rafce" to create the import react lines (ES7+ React/Redux/React-Native snippets extension in VScode)
 import React from 'react'
 import '../Game.css';
 
-
+// Functional component for an individual game circle
 //Parse Properties and Children from GameBoard
 const Gamecircle = ({id, children, className, onCircleClicked}) => {
   return (
+    // Render a circle with a dynamic class and click handler
     // Create Dynamic Class
     <div className={`gameCircle ${className}`}
     // For parsing multiple prometers we need "() =>"
