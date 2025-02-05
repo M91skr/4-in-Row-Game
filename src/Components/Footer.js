c42f3b52b04a60359e590a734e95d774593a352b
Maryam shakeri <89182184+M91skr@users.noreply.github.com>
2025-02-05 12:43:51 +0100

    Update Footer.js
    Improve comments

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,10 +5,12 @@ const Footer = ({ onNewGameClick, onSuggestClick, gameState }) => {
   return (
     <div className="panel footer">
       {
+        // Show the "Suggest" button only if the game is currently being played
         gameState === GAME_STATE_PLAYING &&
         <button onClick={onSuggestClick}>Suggest</button>
       }
       {
+        // Show the "New Game" button when the game is not in the playing state (idle, win, or draw)
         gameState !== GAME_STATE_PLAYING &&
         <button onClick={onNewGameClick}>New Game</button>
       }
